280fbcf9446da8fb8b16c416318f46f1b9b7963b
=Mtg_Dev <=mtg0987654321@gmail.com>
2021-07-21 18:30:52 +0200

    deployment

--- src/utils/wrapper.jsx
+++ src/utils/wrapper.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 function Wrapper(props) {
-  return <Router>{props.children}</Router>;
+  return <Router basename="/what-to-play">{props.children}</Router>;
 }
 
 export default Wrapper;
